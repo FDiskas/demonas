4d60e5a7fd719fd5919005649a5bf8cd8d60f26a
Vytenis <projektas@gmail.com>
2020-02-01 21:02:29 +0200

    Fixed generators

--- plopfile.js
+++ plopfile.js
@@ -43,20 +43,29 @@ module.exports = (
                 type: 'input',
                 name: 'name',
                 message: 'What is your page name?',
+                validate: function(string) {
+                    var done = this.async();
+                    if (string.toLowerCase().indexOf('screen') < 0) {
+                        done(null, true);
+                    }
+
+                    done('"Screen" prefix added automatically');
+                    return;
+                },
             },
             {
                 type: 'confirm',
                 name: 'navigator',
                 message: 'Add to navigation?',
-                default: false,
+                default: true,
             },
         ],
         actions: ({ navigator }) => {
             const actions = [
                 {
                     type: 'add',
-                    path: 'src/screens/{{pascalCase name}}/{{pascalCase name}}.tsx',
-                    templateFile: '.templates/Component/Component.tsx.hbs',
+                    path: 'src/screens/{{pascalCase name}}/{{pascalCase name}}Screen.tsx',
+                    templateFile: '.templates/screens/screen.tsx.hbs',
                 },
                 {
                     type: 'add',
@@ -66,7 +75,7 @@ module.exports = (
                 {
                     type: 'append',
                     path: 'src/screens/index.ts',
-                    template: `export * from './{{pascalCase name}}/{{pascalCase name}}';\n`,
+                    template: `export * from './{{pascalCase name}}/{{pascalCase name}}Screen';\n`,
                     skipIfExists: true,
                 },
             ];
@@ -75,20 +84,20 @@ module.exports = (
                     {
                         type: 'add',
                         path: 'src/routes/index.ts',
-                        templateFile: '.templates/screen.ts.hbs',
+                        templateFile: '.templates/screens/index.ts.hbs',
                         skipIfExists: true,
                     },
                     {
                         type: 'append',
                         path: 'src/routes/index.ts',
                         pattern: `/* PLOP_INJECT_IMPORT */`,
-                        template: `import { {{pascalCase name}} } from 'app/screens';`,
+                        template: `import { {{pascalCase name}}Screen } from 'app/screens/{{pascalCase name}}/{{pascalCase name}}Screen';`,
                     },
                     {
                         type: 'append',
                         path: 'src/routes/index.ts',
                         pattern: `/* PLOP_INJECT_EXPORT */`,
-                        template: `    {{pascalCase name}},`,
+                        template: `    {{pascalCase name}}: { screen: {{pascalCase name}}Screen },`,
                     },
                 );
             }
--- src/components/LoginForm/LoginForm.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import 'react-native';
-import { create } from 'react-test-renderer';
-
-import { LoginForm } from './LoginForm';
-
-it('renders correctly', () => {
-    create(<LoginForm />);
-});
